deb522803683f35ff9ba166c6a979ba53c8f0ee3
natj <nattila.joonas@gmail.com>
2019-12-14 13:41:08 +0100

    day histogram

--- tvisz.py
+++ tvisz.py
@@ -49,6 +49,10 @@ def plot_duration_hist(reports):
     projs = {}
 
     durations = []
+    durations_shallow = []
+    durations_deep = []
+    durations_coll = []
+
     for r in reports:
         tlen = r['duration']/60.0 #duration in minutes
         durations.append(tlen)
@@ -58,26 +62,22 @@ def plot_duration_hist(reports):
         else:
             projs[r['project']] += 1
 
+
+        #deep work
+        if r['project'] in deep_work_labels:
+            durations_deep.append(tlen)
+
+        #shallow work
+        if r['project'] in shallow_work_labels:
+            durations_shallow.append(tlen)
+
+        #collaborative work
+        if r['project'] in collaboration_work_labels:
+            durations_coll.append(tlen)
+
     print(projs)
 
 
-
-    #same for shallow work filtered out
-    durations_deep = []
-    for r in reports:
-        if r['project'] in shallow_work_labels:
-            continue
-
-        tlen = r['duration']/60.0 #duration in minutes
-        durations_deep.append(tlen)
-
-
-    #own projects only
-    durations_own = []
-    for r in reports:
-        if r['project'] in deep_work_labels:
-            tlen = r['duration']/60.0 #duration in minutes
-            durations_own.append(tlen)
 
     #----------------------------
     #setup figure
@@ -94,18 +94,22 @@ def plot_duration_hist(reports):
 
     print("samples:", len(durations))
     print("samples (deep):", len(durations_deep))
-    print("samples (own):",  len(durations_own))
+    print("samples (shallows):", len(durations_shallow))
+    print("samples (collaborative):",  len(durations_coll))
     bins = np.linspace(0.0, 100.0, 50)
 
 
     #hist, bins = np.histogram(durations, bins=100)
     hist, bins = np.histogram(durations, bins=bins)
-    axs[0].plot(bins[:-1], hist)
+    axs[0].plot(bins[:-1], hist, 'k')
 
     hist2, bins = np.histogram(durations_deep, bins=bins)
     axs[0].plot(bins[:-1], hist2, "r-")
 
-    hist3, bins = np.histogram(durations_own, bins=bins)
+    hist2, bins = np.histogram(durations_shallow, bins=bins)
+    axs[0].plot(bins[:-1], hist2, "b-")
+
+    hist3, bins = np.histogram(durations_coll, bins=bins)
     axs[0].plot(bins[:-1], hist3, "g-")
 
 
@@ -125,6 +129,93 @@ def plot_duration_hist(reports):
     fig.savefig('durations.pdf')
 
 
+def find_nearest(array, value):
+    array = np.asarray(array)
+    idx = (np.abs(array - value)).argmin()
+    return idx
+
+
+def plot_day_hist(reports):
+
+    N = 100
+    bins = np.linspace(0.0, 24.0, N)
+
+    hist = np.zeros(N) #all
+    hist_deep = np.zeros(N) #deep work
+    hist_shallow  = np.zeros(N) #shallow work
+    hist_coll = np.zeros(N) #collaborative
+
+
+    for r in reports:
+
+        tstart = r['start']
+        tstop =  r['end']
+        t0 = tstart.hour + tstart.minute/60.0
+        t1 = tstop.hour + tstop.minute/60.0
+
+        idx = find_nearest(bins, t0)
+        while True:
+            if bins[idx] < t1:
+
+                #all
+                hist[idx] += 1
+
+                #deep work
+                if r['project'] in deep_work_labels:
+                    hist_deep[idx] += 1
+
+                #shallow work
+                if r['project'] in shallow_work_labels:
+                    hist_shallow[idx] += 1
+
+                #collaborative
+                if r['project'] in collaboration_work_labels:
+                    hist_coll[idx] += 1
+
+                idx += 1
+            else:
+                break
+
+
+
+
+
+    #----------------------------
+    #setup figure
+    fig = plt.figure(1, figsize=(3.487, 2.5))
+    gs = plt.GridSpec(1, 1)
+    gs.update(hspace=0.3)
+
+    axs = []
+    axs.append(plt.subplot(gs[0, 0]))
+
+    for ax in axs:
+        ax.minorticks_on()
+    #----------------------------
+
+    axs[0].plot(bins, hist, 'k')
+    axs[0].plot(bins, hist_deep, 'r-')
+    axs[0].plot(bins, hist_shallow, 'b-')
+    axs[0].plot(bins, hist_coll, 'g-')
+
+
+
+    axs[0].set_xlabel('Hour of the day')
+    axs[0].set_ylabel('N')
+    axs[0].set_xlim((0, 24))
+
+    axs[0].axvline(x=12.0, linestyle='dashed', color='black')
+
+
+
+    axleft    = 0.18
+    axbottom  = 0.16
+    axright   = 0.96
+    axtop     = 0.95
+    fig.subplots_adjust(left=axleft, bottom=axbottom, right=axright, top=axtop)
+    fig.savefig('day.pdf')
+
+
 
 if __name__ == "__main__":
 
@@ -135,10 +226,12 @@ if __name__ == "__main__":
     plt.rc("axes", labelsize=8)
 
 
+    #read
     reports = read_toggl_reports()
 
-
+    #visualize
     plot_duration_hist(reports)
+    plot_day_hist(reports)
 
 
 
